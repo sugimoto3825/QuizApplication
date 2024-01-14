1419f2cf8f82e7c23f0720a016247a39dc9901d6
明日香 杉本 <sugimoto3825@gmail.com>
2024-01-14 16:55:44 +0900

    更新・削除機能を追加

--- QuizApplication/src/main/java/com/example/demo/controller/QuizController.java
+++ QuizApplication/src/main/java/com/example/demo/controller/QuizController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,8 +10,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.entity.Quiz;
@@ -25,12 +30,15 @@ public class QuizController {
 	@ModelAttribute
 	public QuizForm setUpForm() {
 		QuizForm form = new QuizForm();
+		//回答の初期値は"○"
 		form.setAnswer(true);
 		return form;
 	}
 	
+	//http://localhost:8080/quizにアクセスするとshowListの処理が実行される
 	@GetMapping
 	public String showList(QuizForm quizForm, Model model) {
+		//新規登録モードで初期表示
 		quizForm.setNewQuiz(true);
 		
 		Iterable<Quiz> list = service.selectAll();
@@ -58,5 +66,84 @@ public class QuizController {
 		
 		
 	}
+	
+	//下部エリアの編集ボタンを押下するとshowUpdateの処理が実行される
+	@GetMapping("/{id}")
+	public String showUpdate(QuizForm quizForm, @PathVariable Integer id, Model model) {
+		//idに対応するquizを取得
+		Optional<Quiz> quizOpt = service.selectOneById(id);
+		//mapメソッド（中間操作）
+		//値がある時にだけmakeQuizFormでquiz→quizFormに変換した結果を返す
+		Optional<QuizForm> quizFormOpt = quizOpt.map(t -> makeQuizForm(t));
+		
+		if(quizFormOpt.isPresent()) {
+			quizForm = quizFormOpt.get();
+		}
+		
+		makeUpdateModel(quizForm, model);
+		return "crud";
+	}
+	
+	private void makeUpdateModel(QuizForm quizForm, Model model) {
+		model.addAttribute("id", quizForm.getId());
+		quizForm.setNewQuiz(false);
+		model.addAttribute("quizForm", quizForm);
+		model.addAttribute("title", "更新用フォーム");
+	}
+	
+	@PostMapping("/update")
+	public String update(
+			@Validated QuizForm quizForm,
+			BindingResult result,
+			Model model,
+			RedirectAttributes redirectAttributes
+			) {
+		Quiz quiz = makeQuiz(quizForm);
+		
+		if(!result.hasErrors()) {
+			service.updateQuiz(quiz);
+			redirectAttributes.addFlashAttribute("complete", "更新が完了しました");
+			return "redirect:/quiz/" + quiz.getId(); //redirect:ではURLを指定する
+		} else {
+			makeUpdateModel(quizForm, model);
+			return "crud"; //ビューを返す時はredirect:は付けない
+		}
+	}
+	
+	@PostMapping("/delete")
+	public String delete(
+			@RequestParam("id") String id,
+			Model model,
+			RedirectAttributes redirectAttributes
+			) {
+		service.deleteQuizById(Integer.parseInt(id));
+		redirectAttributes.addFlashAttribute("delcomplete", "削除が完了しました");
+		return "redirect:/quiz";
+	}
+	
+	//Form→Entityへの変換
+	private Quiz makeQuiz(QuizForm quizForm) {
+		Quiz quiz = new Quiz();
+		quiz.setId(quizForm.getId());
+		quiz.setQuestion(quizForm.getQuestion());
+		quiz.setAnswer(quizForm.getAnswer());
+		quiz.setAuthor(quizForm.getAuthor());
+		return quiz;
+	}
+	
+	//Entity→Formへの変換
+	private QuizForm makeQuizForm(Quiz quiz) {
+		QuizForm quizForm = new QuizForm();
+		quizForm.setId(quiz.getId());
+		quizForm.setQuestion(quiz.getQuestion());
+		quizForm.setAnswer(quiz.getAnswer());
+		quizForm.setAuthor(quiz.getAuthor());
+		//更新モードに変更
+		quizForm.setNewQuiz(false);
+		return quizForm;
+	}
+	
+	
+	
 
 }
